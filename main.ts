17265e251d4cc58930ecfc13e68165f757796ed8
Peter Slattery <slattery.peter.j@gmail.com>
2024-06-05 13:34:07 -0700

    Task Update: Write a function that calculates the nth fibonacci number in constant time.

--- main.ts
+++ main.ts
@@ -2,4 +2,9 @@ function main() {
     console.log('hello world!');
 }
 
-main();
+main();```javascript
+function fibonacci(n) {
+    const phi = (1 + Math.sqrt(5)) / 2;
+    return Math.round(Math.pow(phi, n) / Math.sqrt(5));
+}
+```
